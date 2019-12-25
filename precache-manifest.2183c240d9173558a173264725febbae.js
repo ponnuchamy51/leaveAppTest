b11c2595d6970f834561662740aec8f867bc925d
ponnuchamy <tpsamy051@gmail.com>
2019-12-25 23:58:44 +0530

    Updates

---
+++ precache-manifest.2183c240d9173558a173264725febbae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0b2bf94f93e5a7afbceb697b0310d2",
+    "url": "/ponnuchamy51/leaveAppTest/index.html"
+  },
+  {
+    "revision": "4707919cfa17bf85347b",
+    "url": "/ponnuchamy51/leaveAppTest/static/css/2.468c5d8a.chunk.css"
+  },
+  {
+    "revision": "51faed5915d316846af8",
+    "url": "/ponnuchamy51/leaveAppTest/static/css/main.7db88f96.chunk.css"
+  },
+  {
+    "revision": "4707919cfa17bf85347b",
+    "url": "/ponnuchamy51/leaveAppTest/static/js/2.ccca70f4.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/ponnuchamy51/leaveAppTest/static/js/2.ccca70f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51faed5915d316846af8",
+    "url": "/ponnuchamy51/leaveAppTest/static/js/main.bb36f684.chunk.js"
+  },
+  {
+    "revision": "29b7cf84d302b1717c3d",
+    "url": "/ponnuchamy51/leaveAppTest/static/js/runtime-main.4b3ea36c.js"
+  },
+  {
+    "revision": "bacfb255361f0ee0a84cb3e3fe3b5ca1",
+    "url": "/ponnuchamy51/leaveAppTest/static/media/genu.bacfb255.jpg"
+  }
+]);
